a3e2fcdfd2cf9c5bb54f7f9b9389c23cff47b0e6
Arina <hadson.rey@mail.ru>
2020-06-10 16:49:26 +1100

    Добавляет передачу пропс в welcome screen.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/app/app.jsx";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const Settings = {
+  ERRORS_COUNT: 3
+};
+
+ReactDOM.render(<App errorsCount={Settings.ERRORS_COUNT} />, document.getElementById(`root`));

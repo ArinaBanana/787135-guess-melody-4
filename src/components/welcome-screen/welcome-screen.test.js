c2e8dd49a43bbd05b4f338d22ea28476fd44c5c5
Arina <hadson.rey@mail.ru>
2020-06-13 20:21:31 +1100

    Устанавливает enzyme, добавляет обработчик клика по кнопке пуск. Обновляет снепшот тесты.

--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -3,6 +3,6 @@ import renderer from 'react-test-renderer';
 import WelcomeScreen from "./welcome-screen";
 
 it(`Should WelcomeScreen render correctly`, () => {
-  const tree = renderer.create(<WelcomeScreen errorsCount={3} />).toJSON();
+  const tree = renderer.create(<WelcomeScreen errorsCount={3} onWelcomeButtonClick={() => {}} />).toJSON();
   expect(tree).toMatchSnapshot();
 })

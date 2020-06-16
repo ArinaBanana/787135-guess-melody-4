83cf545f40eec548238b42867bbfab793b1ddb20
Arina <hadson.rey@mail.ru>
2020-06-16 23:56:11 +1100

    Переписывает app на классовый компонент .Подключает router: добавляет возможность переключиться на доп. экраны для разработки.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import {Switch, BrowserRouter, Route} from 'react-router-dom';
+
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
 
-const welcomeButtonHandler = () => {};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-const App = (props) => {
-  const {errorsCount} = props;
-  return (
-    <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={welcomeButtonHandler}
-    />
-  );
-};
+  render() {
+    const {errorsCount} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <WelcomeScreen errorsCount={errorsCount} onWelcomeButtonClick={() => {}} />
+          </Route>
+          <Route exact path="/dev-artist">
+            <ArtistQuestionScreen />
+          </Route>
+          <Route exact path="/dev-genre">
+            <GenreQuestionScreen />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,

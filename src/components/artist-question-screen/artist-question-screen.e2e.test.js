b78597c71304df5e6aa506f4e806fa414e434878
Arina <hadson.rey@mail.ru>
2020-06-26 18:56:26 +1100

    Update e2e test Artist Screen

--- src/components/artist-question-screen/artist-question-screen.e2e.test.js
+++ src/components/artist-question-screen/artist-question-screen.e2e.test.js
@@ -31,21 +31,15 @@ configure({adapter: new Adapter()});
 
 it(`Click on user answer should pass to the callback data-object from which this answer was created`, () => {
   const {question} = mock;
+  const {answers} = question;
+  const answer = answers[0];
+
   const onAnswerDone = jest.fn();
-  const userAnswer = {
-    artist: `one`,
-    picture: `pic-one`
-  };
+  const artistQuestion = shallow(<ArtistQuestionScreen question={question} onAnswerDone={onAnswerDone}/>);
 
-  const screen = shallow(<ArtistQuestionScreen onAnswerDone={onAnswerDone} question={question} />);
+  const instance = artistQuestion.instance();
 
-  const answerInputs = screen.find(`input`);
-  const answerOne = answerInputs.at(0);
-
-  answerOne.simulate(`change`, {preventDefault() {}});
-
-  expect(onAnswerDone).toHaveBeenCalledTimes(1);
-
-  expect(onAnswerDone.mock.calls[0][0]).toMatchObject(question);
-  expect(onAnswerDone.mock.calls[0][1]).toMatchObject(userAnswer);
+  expect(instance.state.currentIndexAnswer).toBe(-1);
+  instance._updateAnswer(answer, 0);
+  expect(instance.state.currentIndexAnswer).toBe(0);
 });
